a530f17d23e9d363b4b23088bc963435a03e1337
Aaron Allen <aaronallen8455@gmail.com>
2017-02-26 02:38:06 -0600

    mobile menu working

--- app/code/AAllen/Navigation/view/frontend/web/js/navigation.js
+++ app/code/AAllen/Navigation/view/frontend/web/js/navigation.js
@@ -8,18 +8,47 @@ define([
     'use strict';
 
     return Class.extend({
-        toggle: null,
         swipeArea: $('.navigation'),
+        htmlEl: $('html'),
 
         initialize: function (config, element) {
-            this.toggle = $(element);
-            this.toggle.click(this.open.bind(this));
+            $(element).click(this.toggle.bind(this));
+            this.swipeArea.on('mousedown', this.swipe.bind(this));
 
             return this._super();
         },
 
-        open: function () {
-            this.swipeArea.toggleClass('nav-open');
+        toggle: function () {
+            // open or close the menu
+            if (this.htmlEl.hasClass('nav-open')) {
+                this.htmlEl.removeClass('nav-open');
+                setTimeout(function () {
+                    this.htmlEl.removeClass('nav-before-open');
+                }.bind(this), 300);
+            } else {
+                this.htmlEl.addClass('nav-before-open');
+                setTimeout(function () {
+                    this.htmlEl.addClass('nav-open');
+                }.bind(this), 42);
+            }
+        },
+
+        swipe: function (e) {
+            var xCoord = e.screenX;
+
+            // close the menu on swipe left
+            this.swipeArea.on('mousemove', function (e) {
+                if (xCoord - e.screenX >= 20) {
+                    this.swipeArea.off('mousemove');
+                    this.toggle();
+                } else if (e.screenX > xCoord) {
+                    xCoord = e.screenX;
+                }
+            }.bind(this));
+
+            this.swipeArea.on('mouseup', function () {
+                this.swipeArea.off('mousemove');
+            }.bind(this));
         }
     })
 });
