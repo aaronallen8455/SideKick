acb6a19f2e9a3c097eb99e6dd21554d35a5f4379
Aaron Allen <aaronallen8455@gmail.com>
2017-08-07 22:08:21 -0500

    improved slide show

--- app/code/AAllen/Images/view/frontend/web/js/slideshow.js
+++ app/code/AAllen/Images/view/frontend/web/js/slideshow.js
@@ -11,7 +11,7 @@ define([
             this.imageSources = config.images;
             this.duration = config.duration;
             this.fadeLength = config.fadeLength;
-            this.imageElement = $(elem).children('img').last();
+            this.imageElement = $(elem).children('.slide-show-image-wrapper').last();
             this.container = $(elem);
             this.images = [this.imageElement]; // an array of image elements
 
@@ -23,31 +23,15 @@ define([
          * Display the next image
          */
         changeImage: function (newImage) {
-            //if (newImage[0].height < this.container.outerHeight() - 5) {
-            //    newImage.css('paddingBottom', (this.container.outerHeight() - 5) - newImage[0].height);
-            //}
 
-            //if (!newImage.isAttached) {
-            newImage//.css({maxWidth: window.offsetWidth, maxHeight: this.container.outerHeight() - 5})
-                .appendTo(this.container);
-            //    newImage.isAttached = true;
-            //}
+            this.imageElement.css({'z-index': 50, position: 'absolute'});
+            newImage.appendTo(this.container);
 
-            //newImage.css({position: 'static', opacity: 1, maxWidth: '100%'});
-
-
-            this.imageElement.css(
-                {
-                    maxWidth: this.imageElement[0].width,
-                    position: 'absolute',
-                    marginLeft: (newImage.outerWidth() - this.imageElement.outerWidth()) / 2,
-                    left: newImage[0].offsetLeft
-                }).animate({opacity: 0}, this.fadeLength * 1000, function () {
-                    this.imageElement.detach();
-                    this.imageElement.css({position: 'static', opacity: 1, maxWidth: '100%'});
-                    this.imageElement = newImage;
-                }.bind(this)
-            );
+            this.imageElement.animate({opacity: 0}, this.fadeLength * 1000, function () {
+                this.imageElement.detach();
+                this.imageElement.css({opacity: 1, 'z-index': 0, position: 'static'});
+                this.imageElement = newImage;
+            }.bind(this));
         },
 
         /**
@@ -56,15 +40,19 @@ define([
          */
         nextImage: function () {
             this.currentIndex++;
-            if (this.currentIndex == this.imageSources.length) this.currentIndex = 0;
+            if (this.currentIndex === this.imageSources.length) this.currentIndex = 0;
 
             if (!this.images[this.currentIndex]) {
-                this.images[this.currentIndex] = $('<img>')
-                    .attr('src', this.imageSources[this.currentIndex]);
+
+                var wrapper = $('<div class="slide-show-image-wrapper">'),
+                    image = $('<img>').attr('src', this.imageSources[this.currentIndex]);
+
+                this.images[this.currentIndex] = wrapper.append(image);
+
                 // change image on load
-                this.images[this.currentIndex]
-                    .load(this.changeImage.bind(this, this.images[this.currentIndex]))
+                image.load(this.changeImage.bind(this, this.images[this.currentIndex]))
             } else {
+
                 this.changeImage(this.images[this.currentIndex]);
             }
         },
--- app/code/AAllen/TieredBackground/view/frontend/web/js/scroll.js
+++ app/code/AAllen/TieredBackground/view/frontend/web/js/scroll.js
@@ -12,18 +12,25 @@ define([
             topInitPosition = parseInt(topEl.css('bottom').slice(0,-2)),
             bottomMidRatio = bottomInitPosition / midInitPosition,
             bottomTopRatio = bottomInitPosition / topInitPosition;
+            //animationQueued = false;
 
         function onScroll(e) {
             var yScroll = window.scrollY;
-            window.requestAnimationFrame(function () {
-                // number of pixels to change bottom cloud position by
-                var change = .035 * yScroll;
+            //if (!animationQueued) {
+            //    animationQueued = true;
 
-                // change position of the clouds
-                bottomEl.css('bottom', bottomInitPosition + change);
-                middleEl.css('bottom', midInitPosition + change * bottomMidRatio);
-                topEl.css('bottom', topInitPosition + change * bottomTopRatio);
-            })
+            //    window.requestAnimationFrame(function () {
+                    // number of pixels to change bottom cloud position by
+                    var change = .035 * yScroll;
+
+                    // change position of the clouds
+                    bottomEl.css('bottom', bottomInitPosition + change);
+                    middleEl.css('bottom', midInitPosition + change * bottomMidRatio);
+                    topEl.css('bottom', topInitPosition + change * bottomTopRatio);
+
+            //        animationQueued = false;
+        //        });
+        //    }
         }
 
         $(window).scroll(onScroll);
